017438178cdfb744c34c0decfd6165770de961d7
SovietTesla <hdeux0@att.net>
2017-05-06 21:53:37 -0700

    Trying to figure out GUIs

--- RogueLikeTest/src/GameGUI.java
+++ RogueLikeTest/src/GameGUI.java
@@ -1,5 +1,13 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.event.*;
 
 public class GameGUI extends JFrame {
-
+	
+	public GameGUI(){
+		super("Why chello there");
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 }
