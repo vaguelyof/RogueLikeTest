f2a58ef21c4f65fc50a6871b9b9f0b5d631ac6c9
SovietTesla <hdeux0@att.net>
2017-05-12 06:21:27 -0700

    Added asciiPanel functionality and library

---
+++ RogueLikeTest/src/Game.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+import asciiPanel.AsciiPanel;
+
+public class Game {
+	ArrayList<DungeonLevel> levels;
+	AsciiPanel panel;
+
+	public Game() {
+		levels = new ArrayList<DungeonLevel>();
+	}
+
+	public void addPanel(AsciiPanel p) {
+		panel = p;
+	}
+
+	public void generateNextLevel() {
+		levels.add(new DungeonLevel(levels.size(), 51));
+		levels.get(levels.size() - 1).generateLevel();
+	}
+
+	public void displayMapAroundTile(Tile t, int level) {
+		DungeonLevel d = getLevel(level);
+
+		for (int i = 0; i < panel.getHeightInCharacters(); i++) {
+
+			for (int j = 0; j < panel.getWidthInCharacters(); j++) {
+				int posX = i + t.getX() - panel.getHeightInCharacters()/2;
+				int posY = j + t.getY() - panel.getWidthInCharacters()/2;
+				if (d.getTile(posX,posY) != null) {
+					if (d.getTile(posX, posY).getIsRock()) {
+						panel.setCursorPosition(i, j);
+						panel.write('R');
+
+					} else {
+						panel.setCursorPosition(i, j);
+						panel.write(' ');
+					}
+				}
+			}
+
+		}
+		panel.updateUI();
+	}
+
+	public DungeonLevel getLevel(int d) {
+		if (d < levels.size() && d >= 0) {
+			return levels.get(d);
+		}
+		return null;
+	}
+}
---
+++ RogueLikeTest/src/GameFrame.java
@@ -0,0 +1,43 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class GameFrame extends JFrame {
+	private Game game;
+	private JPanel contentPane;
+
+	public GameFrame(String s, Game g) {
+		super(s);
+		addKeyListener(new KeyHandler());
+		game = g;
+	}
+	
+	private class KeyHandler implements KeyListener{
+		
+		
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println(KeyEvent.getKeyText(e.getKeyCode()) + " pressed");
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
+
+}
