2f012910600d238b4732b5e8673dfbcc9f4bb16a
SovietTesla <hdeux0@att.net>
2017-05-15 10:26:16 -0700

    Added fov

--- RogueLikeTest/src/Game.java
+++ RogueLikeTest/src/Game.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 
 import asciiPanel.AsciiPanel;
 
-
+import squidpony.squidgrid.Radius;
+import squidpony.squidgrid.FOV;
 
 public class Game {
 	private ArrayList<DungeonLevel> levels;
@@ -11,6 +12,7 @@ public class Game {
 	private Player player;
 	private int currentLevel;
 	private ArrayList<String> helpItems;
+
 	public static int NORTH = 0;
 	public static int NORTH_EAST = 1;
 	public static int EAST = 2;
@@ -64,7 +66,10 @@ public class Game {
 			for (int j = 0; j < panel.getWidthInCharacters(); j++) {
 				int posX = i + t.getX() - panel.getHeightInCharacters()/2 - 3;
 				int posY = j + t.getY() - panel.getWidthInCharacters()/2 + 8;
-				if (d.getTile(posX,posY) != null) {
+				ArrayList<Tile> currentlySeenTiles = calcFOV(t.getX(), t.getY(),d);
+				
+				
+				if (d.getTile(posX,posY) != null && !currentlySeenTiles.contains(d.getTile(posX, posY))) {
 					if (d.getTile(posX, posY).getIsRock()) {
 						panel.setCursorPosition(j, i);
 						panel.write('#');
@@ -129,6 +134,41 @@ public class Game {
 			panel.write(helpItems.get(i - 4));
 		}
 		
+	}
+	
+	public ArrayList<Tile> calcFOV(int x, int y, DungeonLevel dun){
+		double[][] fovmap;
+		FOV fov = new FOV(FOV.SHADOW);
+		
+				
+		int startx = x;
+		int starty = y;
+		
+		fovmap = fov.calculateFOV(generateResistances(dun), startx, starty, 8, Radius.SQUARE);
+		
+		
+		ArrayList<Tile> seen = new ArrayList<Tile>();
+		for(int i = 0; i < fovmap.length; i++){
+			for(int j = 0 ; j < fovmap[0].length; j++){
+				if(fovmap[i][j] < 0.4){
+					seen.add(dun.getTile(i, j));
+				}
+			}
+		}
+		return seen;
+	}
+	
+	private double[][] generateResistances(DungeonLevel dun){
+		double [][] map = new double[dun.getMap().length][dun.getMap().length];
+		for(int i = 0; i < dun.getMap().length; i++){
+			for(int j = 0; j < dun.getMap().length; j++){
+				if(dun.getTile(i, j).getIsRock() || dun.getTile(i, j).getTopEntity() instanceof Door){
+					map[i][j] = 1;
+				}
+				
+			}
+		}
+		return map;
 	}
 
 	public DungeonLevel getLevel(int d) {
