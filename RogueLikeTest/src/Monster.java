12d5c6dc7348e215439becb9cb4a816e91ad3918
BenB <basbenjamin@gmail.com>
2017-05-16 10:14:11 -0700

    Implemented die method in Creature class Added act method in Monster class Added getAdjacentTiles in Tile class Made removeEntity public in Tile class

--- RogueLikeTest/src/Monster.java
+++ RogueLikeTest/src/Monster.java
@@ -1,4 +1,15 @@
+public class Monster extends Creature{
 
-public class Monster {
-
+	Monster(String aName, String description, int health, int dmg)
+	{
+		super(aName, description, health, dmg);
+	}
+	
+	public void act()
+	{
+		if(super.getHealth() == 0){
+			super.die();
+			return;
+		}
+	}
 }
