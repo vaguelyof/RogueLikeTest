93e7a1c24d38b6afc749bb50b9958d1d94ce6f43
1839208 <1839208@00-67056.pusd.dom>
2017-05-19 10:22:44 -0700

    Added RevivePotion class

--- RogueLikeTest/src/Potion.java
+++ RogueLikeTest/src/Potion.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 public class Potion implements Item {
 	private Tile myTile;
 	private int myStack;
-	private int healAmount;
+	protected int healAmount;
 	
 	public Potion(){
 		healAmount = 20;
---
+++ RogueLikeTest/src/RevivePotion.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+
+public class RevivePotion extends Potion {
+	
+	public RevivePotion(){
+		healAmount = 50;
+	}
+	
+	public Color getColor() {
+		return new Color(255,180,180);
+	}
+	
+	public boolean canRevive(){
+		return true;
+	}
+}
